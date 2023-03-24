43bf1d38fe7a8344e1786d297a37f41eb2e2eaeb
Tank <tank@gmail.com>
2023-03-24 13:48:55 +0800

    edit APP_ENV

--- config/web.php
+++ config/web.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'APP_ENV'            => env('APP_ENV', 'dev'),
+    'APP_ENV'            => env('APP_ENV', 'prod'),
     'APP_DEBUG'          => env('APP_DEBUG', false),
     // 默认值配置
     'DEFAULT_THEME'      => env('DEFAULT_THEME', ''),
